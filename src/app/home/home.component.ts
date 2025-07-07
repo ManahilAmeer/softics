63dc6acf008cfb6be92992c2ac3a3cbb0a1c9f36
ManahilAmeer <manahil1054@gmail.com>
2025-07-07 13:54:35 +0500

    ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,6 +19,7 @@ export class HomeComponent {
   goToContact() {
     this.router.navigateByUrl('/contact')
   }
+today = new Date().toDateString();
 
   skills = [
     { name: 'Angular', icon: '/icons/angular.svg' },
